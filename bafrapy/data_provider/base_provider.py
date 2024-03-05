cf3e6d1b60f22964f87efac4a52dbfca4cf6ea52
Jose Francisco Castillo Berenguel <jose25berenguel@gmail.com>
2024-03-05 21:53:24 +0100

    Implement data provider for Binance.

---
+++ bafrapy/data_provider/base_provider.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import backoff
+import requests
+
+from dataclasses import dataclass
+from typing import Dict, List
+from datetime import date
+from dataclasses import dataclass, field
+from typing import Generator
+from requests.exceptions import HTTPError
+from abc import ABC, abstractmethod
+from frozendict import frozendict
+
+@dataclass(frozen=True)
+class Resolution:
+    seconds: int
+    name: str
+
+resolutions = frozendict({
+    1       : Resolution(seconds=1, name='1s'),
+    60      : Resolution(seconds=60, name='1m'),
+    300     : Resolution(seconds=300, name='5m'),
+    900     : Resolution(seconds=900, name='15m'),
+    1800    : Resolution(seconds=1800, name='30m'),
+    3600    : Resolution(seconds=3600, name='1h'),
+    14400   : Resolution(seconds=14400, name='4h'),
+    86400   : Resolution(seconds=86400, name='1d')
+})
+
+find_by_name = lambda name: next((res for res in resolutions.values() if res.name == name), None)
+find_by_seconds = lambda seconds: next((res for res in resolutions.values() if res.seconds == seconds), None)
+
+@dataclass
+class ResolutionManager():
+    resolutions: frozendict[Dict[int, Resolution]] = field(default=resolutions)
+
+    @property
+    def names(self) -> List[str]:
+        return [res.name for res in self.resolutions.values]
+
+    def get_resolution_by_name(self, name: str) -> Resolution:
+        return next((res for res in resolutions.values() if res.name == name), None)
+
+    def get_next_resolution(self, current: Resolution) -> Resolution:
+        sorted_resolutions = sorted(self.resolutions.values(), key=lambda res: res.seconds)
+        current_index = sorted_resolutions.index(current)
+        if current_index + 1 >= len(sorted_resolutions):
+            return None
+        return sorted_resolutions[current_index + 1]
+
+
+_give_up = lambda e: e.response is not None and 500 <= e.response.status_code < 600
+
+@dataclass
+class BaseProvider(ABC):
+
+    @abstractmethod
+    def _handle_response(self, response: requests.Response) -> pd.DataFrame:
+        pass
+
+    @backoff.on_exception(backoff.expo, HTTPError, max_tries=4, giveup=_give_up)
+    def _request_data(self, req: requests.Request) -> pd.DataFrame:
+        with requests.Session() as session:
+            prepared_req = session.prepare_request(req)
+            response = session.send(prepared_req, timeout=300)
+            response.raise_for_status()
+            return self._handle_response(response)
+
+    @abstractmethod
+    def list_available_symbols(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def get_data_resolutions(self, symbol: str) -> List[Resolution]:
+        pass
+
+    @abstractmethod
+    def get_first_available_date(self, symbol: str) -> date:
+        pass
+
+    @abstractmethod
+    def get_data(self, symbol: str, start_date: date, end_date: date) -> Generator[pd.DataFrame, None, None]:
+        pass
+
+
---
+++ bafrapy/data_provider/binance_provider.py
@@ -0,0 +1,130 @@
+import requests
+import io
+import zipfile
+import pandas as pd
+
+from decimal import Decimal
+from datetime import datetime, date, timedelta
+from typing import Generator, List, Tuple
+from dataclasses import dataclass
+from bafrapy.data_provider.base_provider import BaseProvider
+
+
+@dataclass
+class BinanceProvider(BaseProvider):
+
+    @property
+    def api_root_endpoint(self):
+        return "https://api.binance.com/api/v3"
+
+    @property
+    def api_info_endpoint(self):
+        return self.api_root_endpoint + "/exchangeInfo"
+
+    @property
+    def api_aggTrades(self):
+        return self.api_root_endpoint + "/aggTrades"
+
+    @property
+    def data_base_URL(self):
+        return "https://data.binance.vision/data"
+
+    @property
+    def data_daily_klines_URL(self):
+        return  self.data_base_URL + "/spot/daily/klines"
+
+    @property
+    def data_monthly_klines_URL(self):
+        return  self.data_base_URL + "/spot/monthly/klines"
+
+    def _build_data_url(self, symbol: str, resolution: str, date: datetime, monthly: bool) -> str:
+        # Current example url https://data.binance.vision/?prefix=data/spot/daily/klines/1INCHBTC/1h/1INCHBTC-1h-2023-05-26.zip
+        if monthly:
+            return  f'{self.data_monthly_klines_URL}/{symbol}/{resolution}/{symbol}-{resolution}-{date.strftime("%Y-%m")}.zip'
+        else:
+            return f'{self.data_daily_klines_URL}/{symbol}/{resolution}/{symbol}-{resolution}-{date.strftime("%Y-%m-%d")}.zip'
+
+    def list_available_symbols(self) -> list[str]:
+        response = requests.get(self.api_info_endpoint)
+        response.raise_for_status()
+        body = response.json()
+        symbols = []
+        for item in body['symbols']:
+            symbols.append(item['symbol'])
+        return symbols
+    
+    def get_data_resolutions(self, symbol: str = "") -> List[str]:
+        pass
+
+    def get_first_available_date(self, symbol: str) -> date:
+        response = requests.get(self.api_aggTrades, params={"symbol": symbol, "fromId": 1})
+        response.raise_for_status()
+
+        day = datetime.fromtimestamp(response.json()[0]['T'] / 1000)
+
+        return date(day.year, day.month, day.day)
+
+    def _data_range_months(self, start_date: date, end_date: date) -> List[date]:
+        if isinstance(start_date, datetime):
+            start_date = start_date.date()
+        if isinstance(end_date, datetime):
+            end_date = end_date.date()
+        months = []
+
+        if start_date == end_date:
+            return [start_date.replace(day=1)]
+
+        month = start_date.replace(day=1)
+        current_date = date.today()
+
+        while month <= end_date:
+            if month.month == current_date.month and month.year == current_date.year and end_date.month == current_date.month:
+                break
+
+            months.append(month)
+            month = (month + timedelta(days=32)).replace(day=1)
+
+        return months
+
+    def get_data_availability(self, symbol: str) -> Tuple[datetime, datetime]:
+        first_date = self.get_first_available_date(symbol)
+        last_date = (datetime.now() - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+        return (first_date, last_date)
+
+    def get_data(self, symbol: str, start_date: date, end_date: date) -> Generator[pd.DataFrame, None, None]:
+        if start_date.year == end_date.year:
+            end_date.replace(month=12)
+
+        availability = self.get_data_availability(symbol)
+        if start_date.year == availability[0].year:
+            start_date = availability[0]
+
+        if end_date.year == availability[1].year:
+            end_date = availability[1]
+
+        data_range = self._data_range_months(start_date, end_date)
+        reqs = []
+        for date in data_range:
+            # TODO evaluate resolutions
+            reqs.append(requests.Request('GET', self._build_data_url(symbol, '1s', date, monthly=True)))
+
+        for req in reqs:
+            yield self._request_data(req)
+
+        return None
+
+    def _handle_response(self, response: requests.Response) -> pd.DataFrame:
+        zip_bytes = io.BytesIO(response.content)
+        with zipfile.ZipFile(zip_bytes) as zf:
+            csv_file_name = zf.namelist()[0]
+            with zf.open(csv_file_name) as csv_bytes:
+                df = pd.read_csv(csv_bytes, header=None, dtype=str)
+                df = df.iloc[:, :6]
+                df.columns = ['time', 'open', 'high', 'low', 'close', 'volume']
+                df[['open', 'high', 'low', 'close', 'volume']] = df[['open', 'high', 'low', 'close', 'volume']].applymap(lambda x: Decimal(x))
+                df['time'] = pd.to_datetime(df['time'].astype(int), unit='ms')
+                df['resolution'] = 1
+        return df
+
+    def get_data_resolutions(self, symbol: str = "") -> List[str]:
+        pass
