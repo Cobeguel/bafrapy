71b0ba527dfb753e8577fe09a45aeaa233781018
Cobeguel <jose25berenguel@gmail.com>
2025-06-04 00:47:48 +0200

    Simplify backtesting exceptions.

--- bafrapy/backtest/exceptions.py
+++ bafrapy/backtest/exceptions.py
@@ -1,28 +1,24 @@
-from dataclasses import dataclass
 
-
-@dataclass
 class BrokerException(Exception):
     pass
 
 
-@dataclass
 class OrderAlreadyExists(BrokerException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"OrderAlreadyExists: order with {self.order_id} id already exists"
 
 
-@dataclass
 class NotEnoughMoney(BrokerException):
-    
     def __str__(self):
         return f"NotEnoughMoney: broker has not enough money"
+  
     
-@dataclass
 class NotEnoughMoneyToExecuteMarketOrder(BrokerException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"""
@@ -30,50 +26,53 @@ class NotEnoughMoneyToExecuteMarketOrder(BrokerException):
 
         """
 
-@dataclass
+
 class NotEnoughQuoteToExecuteMarketOrder(BrokerException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"""
             NotEnoughMoneyToExecute: not enough money to execute sell market order order with id {self.order_id}
         """
 
-@dataclass
+
 class NotEnoughQuote(BrokerException):
-    
     def __str__(self):
         return f"NotEnoughQuote: broker has not enough quote"
     
 
-@dataclass
 class OrderException(Exception):
     pass
 
-@dataclass
+
 class NewOrderNotOpen(OrderException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"NewOrderNotOpen: order with {self.order_id} id is not open"
 
-@dataclass
+
 class UncancellableOrder(OrderException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"UncancellableOrder: order with {self.order_id} id is not cancelable"
 
-@dataclass
+
 class InvalidStateExecutedSimpleOrder(OrderException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"InvalidStateExecutedSimpleOrder: order with {self.order_id} was executed but its state is invalid"
 
-@dataclass
+
 class InvalidStateExecutedCompositeOrder(OrderException):
-    order_id : int
+    def __init__(self, order_id: int):
+        self.order_id = order_id
 
     def __str__(self):
         return f"InvalidStateExecutedCompositeOrder: order with {self.order_id} was executed but its state is invalid"
